86a2e51b32fbea0c0c8cf7ea293e3c3e9288eecd
Alon Alush <alon.alush.pepper@gmail.com>
2019-09-07 20:50:32 +0300

    a

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const nodeFetch = require('node-fetch')
+const redis = require('redis')
+
+const PORT = process.env.POST || 5000
+const REDIS_PORT = process.env.POST || 6379
+
+const client = redis.createClient(REDIS_PORT)
+const app = express()
+
+function setResponse(username, repos){
+    return `<h1> ${username}  -  Number of repos: ${repos} </h1>`
+}
+
+async function getRepos(req, res, next){
+    try{
+        console.log('fetching data')
+        const  {username} = req.params
+        const  response = await nodeFetch(`https://api.github.com/users/${username}`)
+        const data = await response.json()
+        const repos = data.public_repos
+
+        // Set in redis
+        client.setex(username, 3600, repos)
+        res.send(setResponse(username, repos))
+    } catch (err){
+        console.log(err)
+        res.status(500)
+    }
+}
+
+//Cache middleware
+function cache(req, res, next){
+    const {username} = req.params
+
+    client.get(username, (err, data) => {
+        if(err) throw err
+
+        if(data !== null){
+            res.send(setResponse(username, data))
+        } else {
+            next()
+        }
+    })
+}
+
+app.get('/repos/:username', cache, getRepos)
+
+app.listen(5000, ()=>{
+    console.log(`app running in port ${PORT}`)
+})
